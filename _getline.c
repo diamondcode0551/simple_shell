4bd4ca58ae5162b5b603249e4cbb4433dc9c97b0
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-07-23 00:48:27 -0700

    refactored code to meet betty requirements

--- _getline.c
+++ _getline.c
@@ -7,21 +7,21 @@
 ssize_t _getline(char **input)
 {
 	static char buffer[BUFFER_SIZE];
-	static ssize_t buffer_pos = 0, bytes_in_buffer = 0;
+	ssize_t buffer_pos = 0, bytes_in_buffer = 0;
 	ssize_t bytes_read = 0, newline_pos = -1;
 
 	if (!input)
-		return -1;
+		return (-1);
 
-	if (buffer_pos >= bytes_in_buffer) /* Refill buffer if it's empty or at the end */
+	if (buffer_pos >= bytes_in_buffer)
 	{
 		bytes_in_buffer = read(STDIN_FILENO, buffer, BUFFER_SIZE);
 		buffer_pos = 0;
 
 		if (bytes_in_buffer <= 0)
-			return -1; /* Reached EOF or an error occurred */
+			return (-1); /* Reached EOF or an error occurred */
 	}
-	while (buffer_pos + bytes_read < bytes_in_buffer)/* Find the position of the newline character */
+	while (buffer_pos + bytes_read < bytes_in_buffer)
 	{
 		if (buffer[buffer_pos + bytes_read] == '\n')
 		{
@@ -30,17 +30,17 @@ ssize_t _getline(char **input)
 		}
 		bytes_read++;
 	}
-	if (newline_pos == -1) /* Newline not found in the current buffer, read more */
+	if (newline_pos == -1) /* Newline not foun*/
 	{
 		bytes_read = bytes_in_buffer - buffer_pos;
 		newline_pos = bytes_in_buffer;
 	}
 	*input = malloc(newline_pos + 1);/* Allocate memory for the line */
 	if (!*input)
-		return -1; /* Allocation error */
-	for (ssize_t i = 0; i < newline_pos; i++)/* Copy the data from the buffer into the line */
+		return (-1); /* Allocation error */
+	for (ssize_t i = 0; i < newline_pos; i++)/* Copy the data from the buffer*/
 		(*input)[i] = buffer[buffer_pos + i];
 	(*input)[newline_pos] = '\0'; /* Null-terminate the line */
-	buffer_pos += bytes_read + 1; /* Move buffer_pos to the next character after newline */
-	return newline_pos;
+	buffer_pos += bytes_read + 1; /* Move buffer_pos to the next character*/
+	return (newline_pos);
 }
---
+++ path_finder.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+/**
+ * handle_path - gets the location of a command
+ * @command: shell command
+ * Return: path of the command if found, otherwise NULL
+ */
+char *handle_path(char *command)
+{
+	struct stat buf;
+	char *path, *directory, *path_found, *file_path;
+	int dir_size, cmd_size;
+
+	if (command[0] == '/') /* checks for absolute path*/
+	{
+		if (stat(command, &buf) == 0)
+		{
+			path_found = (char *)malloc(_strlen(command) + 1);
+			_strcpy(path_found, command);
+			return (path_found);
+		}
+		return (NULL); /* File not found */
+	}
+	path = _getenv("PATH");
+	if (path == NULL)
+		return (NULL);
+	cmd_size = _strlen(command);
+	directory = strtok(path, ":");
+	dir_size = _strlen(directory);
+	while (directory != NULL)
+	{
+		file_path = malloc(dir_size + cmd_size + 2);
+		_strcpy(file_path, directory);
+		_strcat(file_path, "/");
+		_strcat(file_path, command);
+		if (stat(file_path, &buf) == 0)
+		{
+			path_found = malloc(_strlen(file_path) + 1);
+			_strcpy(path_found, file_path);
+			free(file_path);
+			return (path_found);
+		}
+		free(file_path);
+		directory = strtok(NULL, ":");
+	}
+	return (NULL); /* Command not found in any path */
+}
